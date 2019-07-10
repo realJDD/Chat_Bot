6c24e70338d460b49737126be2ac4411998442fc
geoben <sambendele@gmail.com>
2019-07-09 19:56:03 -0500

    Add new muscle images

---
+++ templates/data.js
@@ -0,0 +1,9 @@
+var data1 = [
+    {
+        "response": "OK! Show the location of the muscles for biceps on the SVG!"
+    },
+    {
+        "info": "biceps",
+        "tab": "location"
+    }
+]
---
+++ templates/test.js
@@ -0,0 +1,6 @@
+document.getElementById("form1").addEventListener('submit', functSubmit);
+
+function functSubmit() {
+  var msg = document.getElementById("input2").value;
+  alert(msg);
+}
